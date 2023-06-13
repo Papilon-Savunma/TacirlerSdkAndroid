1d2f3c0ab6ba2b2df4ec8309b16b3caee06dfa10
Fy1717 <frknyldz8489@gmail.com>
2023-06-13 16:33:43 +0300

    v.0.0.23 updated

--- ExampleApp/app/src/main/java/com/papilon/tacirlersdk/MainActivity.java
+++ ExampleApp/app/src/main/java/com/papilon/tacirlersdk/MainActivity.java
@@ -5,20 +5,37 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import com.example.digitalonboarding.view.pap_WelcomeKvkk;
 
+import java.security.cert.X509Certificate;
 
-import com.example.tacirlerdob_android.view.pap_WelcomeKvkk;
-
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button openSdkButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        openSdkButton = findViewById(R.id.button);
+
+        openSdkButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startLibraryActivity();
+            }
+        });
     }
 
-    public void goButton(View view) {
+    public void startLibraryActivity() {
         Intent intent = new Intent(MainActivity.this, pap_WelcomeKvkk.class);
         intent.putExtra("calling_package", this.getPackageName());
         intent.putExtra("calling_class", this.getClass().getName());
